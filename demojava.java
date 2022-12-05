b7ee31fc3c61da9f34cf11a2bd42ac52cf4e9a06
sonali <ahirwarsonu0709@gmail.com>
2022-12-06 01:36:48 +0530

    update

--- demojava.java
+++ demojava.java
@@ -2,7 +2,7 @@ class demojava{
 public static void main(String [] args){
 	System.out.println("hello");
 public void add(){
-System.out.println("add method")
+System.out.println("add method");
 } 
 }
 }
