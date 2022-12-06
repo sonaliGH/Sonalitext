02b45bf2d06bb7ed76cac5f2a19374f06c8ad13d
sonali <ahirwarsonu0709@gmail.com>
2022-12-06 17:56:38 +0530

    all code added

--- Text.java
+++ Text.java
@@ -1,6 +1,15 @@
 class Test{
 public static void main (String[] args){
 	System.out.println("welcome to java");
+<<<<<<< Updated upstream
 public void add("add method");
+=======
+
+int a=10;
+int b=30;
+int c=0;
+c=a+b;
+System.out.println(c);
+>>>>>>> Stashed changes
 }
 }
